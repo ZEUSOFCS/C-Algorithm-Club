8d8c36b0171aa678dd092583230ec47c0c1b7d61
Dorian Jäva Brown <zeusofohio@gmail.com>
2017-10-16 22:45:33 -0400

    Create Stack.c

---
+++ Stack/Stack.c
@@ -0,0 +1,10 @@
+/********************************************************           
+    * hello -- program to print out "Hello World".         *   
+    *                                                      *   
+    * Author:  FirstName, LastName                         *   
+    *                                                      *   
+    * Purpose:  Demonstration of a simple program.         *   
+    *                                                      *   
+    * Usage:                                               *   
+    *      Runs the program and the message appears.       *   
+    ********************************************************/  
